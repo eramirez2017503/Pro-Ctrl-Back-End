910dd5e2ee3cb7cce5983f07be91d658f96b7a62
Emilio Ramirez <eramirez-2017503@kinal.edu.gt>
2021-05-31 14:44:39 -0600

    Created the main files for the project

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var app = require('./App')
+var port = 3200;
+
+mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb://localhost:27017/ProCtrl', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('El servidor de Node JS esta funcionando');
+        app.listen(port, () => {
+            console.log('Servidor de Express esta corriendo');
+        })
+    })
+    .catch((err) => {
+        console.log('Error al conecctar la BD', err)
+    })
