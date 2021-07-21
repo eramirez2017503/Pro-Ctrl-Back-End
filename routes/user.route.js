dc300fe9c6a964b2e162fbc8c00f4d9de225cbf7
Matthew Reyes <mreyes-20161552@kinal.edu.gt>
2021-07-21 16:03:56 -0600

    routerUser updated

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,10 +8,10 @@ var upload = connectMultiparty({ uploadDir: './uploads/users'});
 
 var api = express.Router();
 
-api.post('/signUp', userController.signUp);
-api.post('/login', userController.login);
-api.put('/updateUser/:id', [mdAuth.ensureAuth], userController.updateUser);
-api.delete('/removeUser/:id', [mdAuth.ensureAuth], userController.removeUser);
+api.post('/signUp', userController.signUp);//YA
+api.post('/login', userController.login);//YA
+api.put('/updateUser/:id', [mdAuth.ensureAuth], userController.updateUser);//YA
+api.put('/removeUser/:id', [mdAuth.ensureAuth], userController.removeUser);//YA
 api.put('/:id/uploadImage', [mdAuth.ensureAuth, upload], userController.uploadImage); //YA
 api.get('/getImage/:fileName', [upload], userController.getImage);// N/A
 
