74a5b992fd0456f8e82b054f1464211e89b8cd75
jgarcia2016377 <jgarcia-2016377@kinal.edu.gt>
2021-07-24 10:29:15 -0600

    Se corrigio course

--- models/course.model.js
+++ models/course.model.js
@@ -6,11 +6,12 @@ var Schema = moongose.Schema;
 var courseSchema = Schema({
     name : String,
     idCourse : String,
-    level : String, 
+    level : Number, 
     description : String, 
     requirements : String,
     password : String,
     imageCourse : String,
+    type : String,
     topics : [{type: Schema.ObjectId, ref: "topic"}],
     administrator : {type: Schema.ObjectId, ref: "user"},
     users : [{type: Schema.ObjectId, ref: "user"}]
