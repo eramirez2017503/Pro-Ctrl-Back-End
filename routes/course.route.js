5af4a8518e22fddbb7eee213f3c2917562f51268
jgarcia2016377 <jgarcia-2016377@kinal.edu.gt>
2021-07-26 12:48:10 -0600

    Se agrego la funcion de inscribir

--- routes/course.route.js
+++ routes/course.route.js
@@ -12,9 +12,16 @@ api.post('/createCourse/:userId', [mdAuth.ensureAuth, mdAuth.validRolAdmin], cou
 api.put('/:userId/updateCourse/:courseId', [mdAuth.ensureAuth, mdAuth.validRolAdmin], courseController.updateCourse);//maestro
 api.post('/:userId/deleteCourse/:courseId', [mdAuth.ensureAuth, mdAuth.validRolAdmin], courseController.deleteCourse); //maestro
 api.get('/getCourseById/:courseId', [mdAuth.ensureAuth], courseController.getCourseById);
-api.get('/listCoursesAdmin/:userId', [mdAuth.ensureAuth, mdAuth.validRolAdmin], courseController.listCoursesAdmin); //maestro
+api.get('/listCoursesAdmin/:userId', [mdAuth.ensureAuth, mdAuth.validRolAdmin || mdAuth.validRolMaestro], courseController.listCoursesAdmin); //maestro
 api.get('/listCoursesUser/:userId', [mdAuth.ensureAuth, mdAuth.validRolAlumno], courseController.listCoursesUser);
 api.post('/:userId/uploadImage/:courseId', [mdAuth.ensureAuth, mdAuth.validRolMaestro], courseController.uploadImage);
 api.get('/getImageCourse/:fileName', [upload], courseController.getImageCourse);
+api.get('/getCoursesPrivates', [mdAuth.ensureAuth, mdAuth.validRolAdmin || mdAuth.validRolMaestro],courseController.listCoursesPrivate);
+api.get('/getlistCoursesPublic',  courseController.listCoursesPublic);
+
+api.put('/:userId/uploadImage/:courseId', [mdAuth.ensureAuth, upload], courseController.uploadImage);
+api.get('/getImageCourse/:fileName', [upload], courseController.getImageCourse);  
+
+api.post('/:userId/inscriptionCourse/:courseId', [mdAuth.ensureAuth, mdAuth.validRolAlumno || mdAuth.validRolAdmin], courseController.inscriptionCourse);
 
 module.exports = api;
