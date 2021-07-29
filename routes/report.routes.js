47ab54a48bb17b2e6df810a2bea187c79c69522f
Angel Par <apar-2016182@kinal.edu.gt>
2021-07-29 16:17:52 -0600

    Cambios en route y controller de report

--- routes/report.routes.js
+++ routes/report.routes.js
@@ -6,7 +6,8 @@ var mdAuth = require('../middleware/authenticated');
 
 var api = express.Router();
 
-api.post('/createReport/:userId', [mdAuth.ensureAuth, mdAuth.validRolAdmin], reportController.createReport);
+api.post('/createReport/:userId', [mdAuth.ensureAuth, mdAuth.validRolAdmin], reportController.createReport);//Admin
+api.post('/createReportUsers/:userId', [mdAuth.ensureAuth], reportController.createReportUsers);//Users
 api.put('/:userId/updateReport/:reportId', [mdAuth.ensureAuth, mdAuth.validRolAdmin], reportController.updateReport);
 api.get('/listReportAdmin/:userId', [mdAuth.ensureAuth, mdAuth.validRolAdmin], reportController.listReportAdmin); //Admin
 api.get('/listReportUser/:userId', [mdAuth.ensureAuth], reportController.listReportUser); //Usuarios
